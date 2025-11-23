de7e40f8c130096789e7648984238704ee18eaf1
Francisco Gonzalez <franga19.alfaro@gmail.com>
2025-11-22 22:06:08 -0600

    implementacion

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.json({ message: "API funcionando correctamente!" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`API corriendo en puerto ${PORT}`));
